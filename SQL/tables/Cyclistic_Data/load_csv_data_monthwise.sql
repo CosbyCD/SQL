c98e90cddce34fdfb7c0b72dd7dffa9406b0fbd3
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 14:09:43 -0500

    Update load_csv_data_monthwise.sql

--- SQL/tables/Cyclistic_Data/load_csv_data_monthwise.sql
+++ SQL/tables/Cyclistic_Data/load_csv_data_monthwise.sql
@@ -1,45 +1,7 @@
 /*
 File: load_csv_data_monthwise.sql
 
-Description:
-This code snippet orchestrates a dynamic data transfer 
-process, facilitating the ingestion of information from 
-CSV files into respective database tables. The procedure 
-iterates through each month of the year and efficiently 
-populates the designated "cyclistic_data_" tables with 
-data extracted from corresponding CSV files.
-
-Procedure:
-
-The procedure commences by traversing the months, from 
-January (1) to December (12), extracting data for each 
-specific month. Within the loop, the procedure constructs 
-the target table name and CSV file path dynamically based 
-on the current month number. Utilizing the "EXECUTE" 
-command, the code executes a formatted SQL query to 
-efficiently copy data from the CSV file into the 
-designated table. The "COPY" operation is adeptly 
-customized with the table name, CSV file path, and 
-relevant formatting specifications.
-
-Key Elements:
-
-month_num: Integer variable representing the current 
-month's numerical value.
-table_name: Text variable that holds the name of the 
-target table, formed using the month number.
-csv_path: Text variable containing the file path of 
-the CSV file for the current month.
-Objective:
-
-This script is an essential component of the data 
-transformation process. By dynamically copying data 
-from corresponding CSV files into distinct tables 
-for each month, the code streamlines the organization 
-and storage of cycling trip data. This systematic 
-approach facilitates subsequent data analysis and 
-exploration, contributing to the comprehensive 
-evaluation of ridership patterns and trends.
+Executes a loop to import data from 12 separate files into corresponding tables using the COPY command.
 */
 
 -- Loop through the months and copy data from CSV files
