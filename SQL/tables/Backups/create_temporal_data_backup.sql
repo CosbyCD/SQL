3399afc1944860a466abb4a933d90f711c7c4259
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 14:03:52 -0500

    Update create_temporal_data_backup.sql

--- SQL/tables/Backups/create_temporal_data_backup.sql
+++ SQL/tables/Backups/create_temporal_data_backup.sql
@@ -1,17 +1,7 @@
 /*
 File: create_temporal_data_backup.sql
 
-To safeguard data integrity and provide a safety net for
-potential changes, I will be generating a backup table.
-This involves duplicating the structure and content 
-to preserve its original state.
-
-The process includes utilizing the "CREATE TABLE" command to
-establish a new backup table, followed by the "SELECT * FROM"
-statement to copy the data from the respective source table.
-By executing these commands, I will create a backup table 
-ensuring that the table is securely preserved for 
-reference and restoration purposes.
+Creating data backup to safeguard data integrity and provide a safety net for potential changes.
 */
 
 
