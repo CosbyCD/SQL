ebb05c81392e34bb31eb327c9adbcebc9ab98a9c
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 13:05:40 -0500

    Update mode_day_yearly.sql

--- SQL/analysis/Cyclistic/mode_day_yearly.sql
+++ SQL/analysis/Cyclistic/mode_day_yearly.sql
@@ -1,18 +1,8 @@
 /*
 File: mode_day_yearly.sql
 
-This SQL query offers a detailed exploration of ride length
-trends for "Member" and "Casual" users throughout the year.
-By analyzing the "user_data" dataset, it identifies the 
-day of the week with the highest ride activity and computes 
-the average ride duration in minutes for each user type on 
-that particular day. The results are accompanied by the 
-respective weekday, enabling a precise comparison of ride 
-behaviors between user categories. This inquiry into the 
-annual peak ridership day enhances the understanding of 
-user engagement patterns and provides valuable insights 
-for strategic planning and optimizing the overall user 
-experience.
+Segments user ride data by the most frequent day of the week, providing the average ride length in minutes
+for each day, categorized by user type over the course of a year.
 */
 
 SELECT
