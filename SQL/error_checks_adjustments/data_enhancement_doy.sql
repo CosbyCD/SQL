1f5133f698d1ef2ade8cbbae2999df3fcbda68e9
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 14:00:33 -0500

    Update data_enhancement_doy.sql

--- SQL/error_checks_adjustments/data_enhancement_doy.sql
+++ SQL/error_checks_adjustments/data_enhancement_doy.sql
@@ -1,29 +1,11 @@
 /*
 File: data_enhancement_doy.sql
 
-Enhancing Data Understanding and Analysis with 'Day of Year'
-As the project advanced through the stages of table creation, 
-data loading, and migration validation, a thorough evaluation 
-of the resulting dataset revealed avenues for data refinement. 
-Notably, an opportunity emerged to amplify the dataset's 
-temporal context.
-
-Recognizing the potential for enriched insights, the project 
-strategically incorporated the "day_of_year" column. This 
-addition introduces a succinct representation of each day's 
-position within the year, expressed numerically on a scale 
-from 1 to 365. By providing this valuable temporal perspective, 
-the project enhances data interpretability and fosters more 
-sophisticated temporal analyses.
-
-These enhancements underscore a steadfast commitment to robust 
-and comprehensive data practices. Introducing meaningful 
-attributes, the dataset is now primed to deliver heightened 
-insights, fostering informed decision-making and establishing 
-a resilient cornerstone for analytical pursuits.
+Adds a new column 'day_of_year' to the 'user_data' table, then updates it by extracting the 
+day of the year from the 'started_at' column for each record in 'user_data'.
 */ 
 
--- Assuming 'user_data' is the name of your table
+
 ALTER TABLE user_data
 ADD COLUMN day_of_year INTEGER;
 
