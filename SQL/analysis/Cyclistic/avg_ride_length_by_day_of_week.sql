ae4404ecf309a26f4da5baa26ddcbf90cead8198
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 11:58:42 -0500

    Update avg_ride_length_by_day_of_week.sql

--- SQL/analysis/Cyclistic/avg_ride_length_by_day_of_week.sql
+++ SQL/analysis/Cyclistic/avg_ride_length_by_day_of_week.sql
@@ -1,27 +1,17 @@
 /*
 File: avg_ride_length_by_day_of_week.sql
 
-This query performs an insightful analysis of ride-sharing data, 
-specifically focusing on the average ride lengths for different 
+Determining the average ride length for different 
 user categories (i.e., "Member" and "Casual") across different 
 days of the week. The query employs a crosstab approach to 
 present the results in a structured pivot table format.
 
-The analysis begins by categorizing ride data into "Member" and 
-"Casual" categories and extracting the day of the week from the 
-timestamp of each ride. It then calculates the average ride length
-for each combination of user category and day of the week, rounding 
-the results to four decimal places. The data is grouped and ordered
-to ensure meaningful presentation.
+By categorizing ride data into "Member" and "Casual" categories,
+it extracts the day of the week from the timestamp of each ride.
+Then calculates the average ride length for each combination of
+user category and day of the week, rounding the results to four
+decimal places.
 
-The resulting pivot table offers a comprehensive overview of how
-ride lengths vary based on user category and day of the week. 
-This analysis provides valuable insights into user behavior 
-patterns and preferences, which can inform strategic decisions aimed 
-at optimizing ride-sharing services. For instance, the presented data
-could aid in tailoring marketing campaigns, service offerings, and 
-resource allocation to enhance user satisfaction and engagement 
-across different user segments and days of the week.
 */
 
 
