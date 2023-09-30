5dde5fa70521b434cceb11804c22e60d08192c7d
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 13:30:09 -0500

    Update ridership_with_holidays_order_by_casual_member_average.sql

--- SQL/analysis/Holidays/ridership_with_holidays_order_by_casual_member_average.sql
+++ SQL/analysis/Holidays/ridership_with_holidays_order_by_casual_member_average.sql
@@ -1,15 +1,7 @@
 /*
 File: ridership_with_holidays_order_by_casual_member_average.sql
 
-This query provides insights into ride patterns and preferences for
-"Member" and "Casual" users, allowing a comprehensive understanding
-of user behavior throughout the year. By leveraging data from the 
-"error_free_records" dataset and optional holiday information, the query
-calculates counts and average counts of rides for both user types. 
-User population sizes are considered, ensuring fair comparisons 
-between segments. The analysis is organized by date, day of the 
-year, day name, and potential holiday name, and ordered by average
-casual riders in decending order.
+Summary of daily ridership, including counts and averages for each segmented group, along with holiday information.
 */
 
 SELECT
