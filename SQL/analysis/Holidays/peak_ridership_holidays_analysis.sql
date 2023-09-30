fe48ad89fda3d8d4048ceeb4dc025f55876d8bb2
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 13:26:49 -0500

    Update peak_ridership_holidays_analysis.sql

--- SQL/analysis/Holidays/peak_ridership_holidays_analysis.sql
+++ SQL/analysis/Holidays/peak_ridership_holidays_analysis.sql
@@ -1,13 +1,7 @@
 /*
 File: peak_ridership_holidays_analysis.sql
 
-The provided query analyzes peak ridership during holidays by 
-calculating the total ridership (user casual and member 
-riders) for each holiday. It excludes rows with NULL holiday 
-names, ensuring only valid holiday data is considered. The 
-results offer insights into which holidays attract the highest
-overall ridership, aiding in understanding the popularity of 
-specific holidays among riders.
+Total user counts for each holiday, segmented by group.
 */
 
 SELECT
