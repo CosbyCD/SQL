08d7c43d7efab25dd217a04b6dd8acae82c4e378
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 12:06:35 -0500

    Update count_riders_per_month_yearly.sql

--- SQL/analysis/Cyclistic/count_riders_per_month_yearly.sql
+++ SQL/analysis/Cyclistic/count_riders_per_month_yearly.sql
@@ -1,16 +1,12 @@
 /*
 File: count_riders_per_month_yearly.sql
 
-This SQL query employs the crosstab function to transform ride
-count data from the user_data table. The query calculates 
-and categorizes the number of rides undertaken by both member 
-and casual riders for each month of the year. The pivot table 
-generated from this query offers a concise summary, allowing 
-analysis of monthly ride patterns and a comparison of ride counts
-between member and casual riders. This information can be 
-valuable for understanding rider engagement trends and making
-informed decisions related to resource allocation and service 
-enhancements.
+Employing the crosstab function to transform ride
+count data from the user_data table. Calculating and categorizing
+the number of rides undertaken by both member and casual riders 
+for each month of the year. The pivot table generated from this query
+allows analysis of monthly ride patterns and a comparison of ride counts
+between member and casual riders.
 */
 
 
