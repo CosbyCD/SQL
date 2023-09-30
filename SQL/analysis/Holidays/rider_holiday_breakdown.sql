7fb07a2dec27fe77a03f02485f880e53e9c84e4f
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 13:29:27 -0500

    Update rider_holiday_breakdown.sql

--- SQL/analysis/Holidays/rider_holiday_breakdown.sql
+++ SQL/analysis/Holidays/rider_holiday_breakdown.sql
@@ -1,17 +1,7 @@
 /*
 File: rider_holiday_breakdown.sql
 
-The provided query conducts an analysis of ridership behavior 
-in relation to holidays. By employing the data from the 
-"error_free_records" dataset and incorporating the "holidays" 
-table through a left join, the query calculates the count 
-of both casual and member riders for each holiday. The 
-grouped results offer insights into the distribution of 
-rider types during various holidays. The outcomes, ordered
-by holiday name, facilitate a comprehensive view of how 
-rider engagement varies across different holidays, aiding
-in understanding the preferences and behaviors of riders 
-during these occasions.
+Total counts for each holiday, segmented by user type.
 */
 
 SELECT
