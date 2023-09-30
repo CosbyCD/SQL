7e3f7e72968b4978d260abb2d6463657ec1810b3
Cherrie D Cosby <cherriecosby@gmail.com>
2023-09-30 14:14:29 -0500

    Update load_weather_data.sql

--- SQL/tables/Third_Party_Data/load_weather_data.sql
+++ SQL/tables/Third_Party_Data/load_weather_data.sql
@@ -1,11 +1,7 @@
 /*
 File: load_weather_data.sql
 
-The query efficiently loads data from a CSV file into the 
-"weather_data" table using the PostgreSQL COPY command. 
-This action facilitates the transfer of weather data from
-the specified file to the database table, enabling subsequent
-analysis and processing.
+Copies data from the specified CSV file into the 'weather_data' table.
 */
 
 
